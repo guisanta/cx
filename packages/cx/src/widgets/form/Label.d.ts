97ff9a804a1eae3999717fab5cbb327c77dcc179
Ranka Arsenovic <rannkaa@gmail.com>
2017-03-31 14:44:36 +0200

    Label prop fix

--- packages/cx/src/widgets/form/Label.d.ts
+++ packages/cx/src/widgets/form/Label.d.ts
@@ -1,12 +1,12 @@
 import * as Cx from '../../core';
 
 interface LabelProps extends Cx.HtmlElementProps{
+   required?: 
+   disabled?:
+   htmlFor?:
    
    /** Base CSS class to be applied to the element. No class is applied by default. */
    baseClass?: string,
-
-   /** Name of the HTML element to be rendered. Default is `div`. */
-   tag?: string,
 
    /** Set to `true` to add red asterisk for required fields. */
    asterisk?: boolean
